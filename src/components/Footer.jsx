d8da50340a5b16b227de2d7c24440b5fb2d7bee1
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-08-17 21:23:49 +0530

    Created Footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import "boxicons/css/boxicons.min.css";
+
+const Footer = () => {
+  return (
+    <footer className="flex items-center justify-between lg:mt-[15%] mt-[25%] py-8 lg:px-32 md:px-16 px-8 border-t-[0.3px] border-[#babaff] bg-black">
+      <img className="h-10" src="public\images\illu-text.png" alt="Illu-text" />
+      <img
+        className="h-16 md:inline hidden"
+        src="public\images\illu-logo.png"
+        alt="Illu-logo"
+      />
+      <div className="flex gap-4">
+        <a
+          href="#"
+          className="md:text-3xl text-2xl hover:text-violet-600 duration-300"
+        >
+          <i class="bx bxl-instagram"></i>
+        </a>
+        <a
+          href="#"
+          className="md:text-3xl text-2xl hover:text-violet-600 duration-300"
+        >
+          <i class="bx bxl-linkedin-square"></i>
+        </a>
+        <a
+          href="#"
+          className="md:text-3xl text-2xl hover:text-violet-600 duration-300"
+        >
+          <i class="bx bxl-github"></i>
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
